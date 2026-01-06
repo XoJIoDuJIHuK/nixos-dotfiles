fe7c66b37e60c41db6ee79a2d70bd44ef01375e6
Aleh Tachyla <tochilo.oleg@yandex.by>
2026-01-06 18:18:06 +0300

    Add timeouts to vim to reduce lag

--- configs/nvim/lua/config/options.lua
+++ configs/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Faster timeout for which-key popup (reduce lag when pressing Esc)
+vim.opt.timeoutlen = 150
+-- Longer ttimeoutlen to prevent Esc+j/k from registering as Alt+j/k
+vim.opt.ttimeoutlen = 100
