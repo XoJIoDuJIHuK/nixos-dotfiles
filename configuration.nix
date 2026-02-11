1a7f322bb6099d195271369302f4e1d026797936
Aleh Tachyla <tochilo.oleg@yandex.by>
2026-02-11 18:37:03 +0300

    Added ttl 65

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,11 @@
       efi.canTouchEfiVariables = true;
     };
     kernelParams = [ "console=tty1" ];
+    kernel.sysctl = {
+      "net.ipv4.ip_default_ttl" = 65;
+      "net.ipv6.conf.all.hop_limit" = 65;
+      "net.ipv6.conf.default.hop_limit" = 65;
+    };
   };
 
 
