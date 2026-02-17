736386397c6e9febe509ad12081dc5d043ced4a4
Aleh Tachyla <tochilo.oleg@yandex.by>
2026-02-17 13:31:40 +0300

    Printing updates

--- configuration.nix
+++ configuration.nix
@@ -117,7 +117,7 @@
       "video"
       "bluetooth"
       "docker"
-      "lpadmin"
+      "lpadmin"  # for printing without sudo
     ];
   };
 
@@ -160,7 +160,22 @@
   ];
 
   services.tailscale.enable = true;  # single `sudo tailscale up` is needed afterwards
-  services.printing.enable = true; # CUPS service for document frinting
+
+  services.printing = {     # http://localhost:631/admin
+    enable = true;          # CUPS service for document frinting
+    browsed.enable = false; # disable cups-browsed (security vulns in 2024 + prevents buggy auto-queues that point to /dev/null)
+    drivers = with pkgs; [  # just in case of missing drivers issues
+      gutenprint            # very broad support
+      # hplip               # HP printers
+      # brlaser             # some Brother lasers
+    ];
+  };
+  # mDNS / .local discovery (required for most network printers)
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;   # IPv4 .local resolution (avoids IPv6 issues)
+    openFirewall = true;  # allows UDP 5353 for discovery
+  };
 
   # Do not change this value
   system.stateVersion = "25.05";
