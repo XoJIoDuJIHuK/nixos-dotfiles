a339514c817a70c00c2f9826ef535f85b3c1b574
Aleh Tachyla <tochilo.oleg@yandex.by>
2026-03-05 08:47:35 +0300

    Commented out markdown preview for nvim

--- configs/nvim/lua/plugins/example.lua
+++ configs/nvim/lua/plugins/example.lua
@@ -468,18 +468,18 @@ return {
     end,
   },
 
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-    keys = {
-      { "<leader>m", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview", ft = "markdown" },
-    },
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = "cd app && npm install",
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { "markdown" }
+  --   end,
+  --   ft = { "markdown" },
+  --   keys = {
+  --     { "<leader>m", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview", ft = "markdown" },
+  --   },
+  -- },
 
   {
     "antosha417/nvim-lsp-file-operations",
